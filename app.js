d57d309ad9aab8c8a753765365797d331a50d964
Shivam Mishra <mshiv619@gmail.com>
2021-06-19 01:06:13 +0530

    updated input output
    added API and various event handlers to make it responsive

--- app.js
+++ app.js
@@ -1,2 +1,31 @@
 var btnTranslate = document.querySelector("#btn-translate");
-console.log(btnTranslate)
+var txtInput = document.querySelector("#txt-input");
+var outputDiv = document.querySelector("#output")
+
+var serverURL = "https://api.funtranslations.com/translate/morse.json"
+
+
+function getTranslationURL(text) {
+    return serverURL + "?" + "text=" + text
+}
+
+function errorHandler(error) {
+    console.log("error occured", error);
+    alert("OOPS!!  Something went wrong OR requests per hour exceeded.")
+    alert("Please try after some time.")
+}
+
+function clickHandler() {
+    var inputText = txtInput.value;
+
+    // calling server
+    fetch(getTranslationURL(inputText))
+        .then(response => response.json())
+        .then(json => {
+            var translatedText = json.contents.translated;
+            outputDiv.innerText = translatedText; // output
+        })
+        .catch(errorHandler)
+};
+
+btnTranslate.addEventListener("click", clickHandler)
